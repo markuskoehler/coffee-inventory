ebca7ef2b785e37d612f00f999f1eada0569c2b2
Markus Köhler <info@koehler.pro>
2017-11-13 16:02:55 +0100

    fixed resource controller route registration

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resources([
-	'coffee' => \App\Http\Controllers\CoffeeController::class,
-	'images' => \App\Http\Controllers\ImageController::class,
-	'locations' => \App\Http\Controllers\LocationController::class,
-	'machines' => \App\Http\Controllers\MachineController::class,
+	'coffee' => 'CoffeeController',
+	'images' => 'ImageController',
+	'locations' => 'LocationController',
+	'machines' => 'MachineController',
 ]);
