90fa15716584b79770cc6df7433a22c2a299e048
Markus Köhler <info@koehler.pro>
2017-11-18 17:14:31 +0100

    improved locations index: aligned add icon, display all fields, use forms macro

--- resources/views/user/locations/index.blade.php
+++ resources/views/user/locations/index.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1>All the Locations</h1>
-
-        <a class="btn btn-small btn-success" href="{{ URL::to('locations/create') }}">Add</a>
+        <h1>All the Locations <a class="btn btn-small btn-success pull-right" href="{{ URL::to('locations/create') }}"><i class="fa fa-plus" aria-hidden="true"></i></a></h1>
 
         <!-- will be used to show any messages -->
         @if (Session::has('message'))
@@ -16,8 +14,8 @@
             <tr>
                 <td>ID</td>
                 <td>Name</td>
-                <td>Email</td>
                 <td>Address</td>
+                <td>Room</td>
                 <td>Actions</td>
             </tr>
             </thead>
@@ -26,8 +24,8 @@
                 <tr>
                     <td>{{ $value->id }}</td>
                     <td>{{ $value->name }}</td>
-                    <td>{{ $value->email }}</td>
-                    <td>{{ $value->address }}</td>
+                    <td>{{ $value->address }}<br>{{ $value->zip }} {{ $value->place }}</td>
+                    <td>{{ $value->room }}</td>
 
                     <!-- we will also add show, edit, and delete buttons -->
                     <td>
@@ -36,14 +34,15 @@
                         <!-- we will add this later since its a little more complicated than the other two buttons -->
                         {{ Form::open(array('url' => 'locations/' . $value->id, 'class' => 'pull-right')) }}
                             {{ Form::hidden('_method', 'DELETE') }}
-                            {{ Form::submit('Delete', array('class' => 'btn btn-warning')) }}
+                            {{-- Form::submit('<i class="fa fa-trash-o" aria-hidden="true"></i>', array('class' => 'btn btn-danger')) --}}
+                            {!!  Form::faSubmit('danger', 'trash-o') !!}
                         {{ Form::close() }}
 
                         <!-- show the location (uses the show method found at GET /locations/{id} -->
-                        <a class="btn btn-small btn-success" href="{{ URL::to('locations/' . $value->id) }}">Show</a>
+                        <a class="btn btn-small btn-success" href="{{ URL::to('locations/' . $value->id) }}"><i class="fa fa-eye" aria-hidden="true"></i></a>
 
                         <!-- edit this location (uses the edit method found at GET /locations/{id}/edit -->
-                        <a class="btn btn-small btn-info" href="{{ URL::to('locations/' . $value->id . '/edit') }}">Edit</a>
+                        <a class="btn btn-small btn-info" href="{{ URL::to('locations/' . $value->id . '/edit') }}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
 
                     </td>
                 </tr>
