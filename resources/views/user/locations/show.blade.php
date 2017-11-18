f6746d133ca573d8a9d7042043891cd65345519e
Markus Köhler <info@koehler.pro>
2017-11-18 17:14:48 +0100

    show all fields in location show blade

--- resources/views/user/locations/show.blade.php
+++ resources/views/user/locations/show.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="container">
-        <h1>Showing {{ $location->name }}</h1>
+        <h1>Showing Location: {{ $location->name }}</h1>
 
         <div class="jumbotron text-center">
             <h2>{{ $location->name }}</h2>
             <p>
-                <strong>Email:</strong> {{ $location->zip }}<br>
-                <strong>Level:</strong> {{ $location->address }}
+                {{ $location->address }}, {{ $location->zip }} {{ $location->place }}<br>
+                <strong>Room:</strong> {{ $location->room }}
             </p>
         </div>
     </div>
